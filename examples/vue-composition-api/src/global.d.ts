70e68d6f72766be78448a303cc1843c4b576925e
Divyanshu Maithani <div.blackcat@gmail.com>
2025-02-03 19:29:56 +0530

    remove global definitions

--- examples/vue-composition-api/src/global.d.ts
+++
@@ -1,8 +0,0 @@
-// import PSPDFKit from "pspdfkit";
-
-// declare global {
-//   interface Window {
-//     // Nutrient Web SDK will be available on window.PSPDFKit once loaded via CDN script
-//     PSPDFKit?: typeof PSPDFKit;
-//   }
-// }
