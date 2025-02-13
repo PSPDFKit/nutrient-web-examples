74ba10253e82ec5900ddbc26ea59454887bf64e7
Miguel Calderón <miguel@pspdfkit.com>
2025-02-13 20:14:34 +0100

    Indentation.

--- scripts/audit-dependencies.sh
+++ scripts/audit-dependencies.sh
@@ -10,10 +10,10 @@ for dir in examples/*; do
 
         echo -e "\n\033[34;1m* $dir:\033[0m"
 
-		pushd "${SCRIPT_DIR}/../${dir}/" > /dev/null
+        pushd "${SCRIPT_DIR}/../${dir}/" > /dev/null
 
         result=0
-		initialresult=0
+        initialresult=0
 
         if [ -f "pnpm-lock.yaml" ]; then
             # Check if the 'total' field exists and extract it if present
@@ -36,19 +36,19 @@ for dir in examples/*; do
         # If vulnerabilities were found, output the result
 
         if (( initialresult > 0 )); then
-			((fixed = initialresult - result))
+            ((fixed = initialresult - result))
             echo -e "  \033[93mFound:     ${initialresult}\033[0m"
             echo -e "  \033[92mFixed:     $fixed\033[0m"
 
-			if (( result > 0 )); then
-	            echo -e "  \033[31mRemaining: ${result}\033[0m"
-			else
-				echo -e "  \033[92;1mAll fixed!\033[0m"
-			fi
-		else
-			echo -e "  \033[34mNo npm vulnerabilities found\033[0m"
+            if (( result > 0 )); then
+                echo -e "  \033[31mRemaining: ${result}\033[0m"
+            else
+                echo -e "  \033[92;1mAll fixed!\033[0m"
+            fi
+        else
+            echo -e "  \033[34mNo npm vulnerabilities found\033[0m"
         fi
 
-		popd > /dev/null
+        popd > /dev/null
     fi
 done
