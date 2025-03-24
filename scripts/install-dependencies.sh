17b05e0ebc7b84dce97ec95d03e5b447cc43a696
Miguel Calderón <miguel@pspdfkit.com>
2025-03-24 08:37:55 +0100

    Update dependencies install script.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+# Runs npm/pnpm install on all examples
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for dir in examples/*; do
   if [ -d "$dir" ]; then
-    echo "Running e2e tests in $dir"
-    (cd "$dir" && npm install)
+    echo "Installing dependencies in $dir"
+
+    pushd "${SCRIPT_DIR}/../${dir}/" > /dev/null
+
+    if [ -f "pnpm-lock.yaml" ]; then
+      pnpm install
+    elif [ -f "package-lock.json" ]; then
+      npm install
+    fi
+
+    popd > /dev/null
+
   fi
 done
