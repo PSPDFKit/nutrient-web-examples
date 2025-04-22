5f196b5da6091410975a0aeb08e7af8e6a32e600
veroo-m <veronica@pspdfkit.com>
2025-04-22 12:27:08 +0200

    fix errors in scripts

--- scripts/update-version-in-gatsby.js
+++ scripts/update-version-in-gatsby.js
@@ -1,23 +1,22 @@
 const fs = require("node:fs");
 const path = require("node:path");
+const { execSync } = require("child_process");
 
 const viewportTemplate = fs.readFileSync(
   path.resolve("./examples/gatsbyjs/src/templates/Viewport.js"),
-  "utf8",
+  "utf8"
 );
 
-const version = execSync("npm view @nutrient-sdk/viewer version")
-  .toString()
-  .trim();
+const version = execSync("npm view @nutrient-sdk/viewer version").toString().trim();
 
 const updatedViewportTemplate = viewportTemplate.replace(
   /pspdfkit-web@([0-9]+.[0-9]+.[0-9]+)?/g,
-  `pspdfkit-web@${version}`,
+  `pspdfkit-web@${version}`
 );
 
 fs.writeFileSync(
   path.resolve("./examples/gatsbyjs/src/templates/Viewport.js"),
-  updatedViewportTemplate,
+  updatedViewportTemplate
 );
 
 console.log("Updated version in Gatsby example.");
