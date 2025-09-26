e38693ee1919a7078a1a6b54a730b5ff11312e12
Sasha <sasha.milenkovic@nutrient.io>
2025-09-26 12:38:55 -0400

    fix: removes excludes from vite config

--- frameworks/vue/TS/vite.config.ts
+++ frameworks/vue/TS/vite.config.ts
@@ -4,7 +4,4 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  optimizeDeps: {
-    exclude: ["@nutrient-sdk/viewer"],
-  },
 });
